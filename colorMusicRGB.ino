932ec1f5f20456720753793fd09c02705905e4d3
urevoleg <urevolegg@gmail.com>
2020-04-16 22:53:21 +0300

    add 2 modes, 1 - audio 2 - colorWheel, setup by encoder

--- colorMusicRGB.ino
+++ colorMusicRGB.ino
@@ -23,6 +23,24 @@ unsigned long last_update_max_value = 0;
 
 #define SAMPLES 250
 
+#define USE_ENCODER 0 // управление при помощи энкодера
+#define USE_BTN 0     // управление при помощи кнопки
+
+#ifdef USE_ENCODER
+#define BTN_PIN 10
+#define CLK 4
+#define DT 9
+#include "GyverEncoder.h"
+Encoder enc1(CLK, DT, BTN_PIN);
+#endif
+
+#ifdef USE_BTN
+#define BTN_PIN 10
+#endif
+
+
+
+//############### ПАРАМЕТРЫ РЕЖИМА audioRGB #######################
 /*
    Параметры сглаживания средней громкости
 */
@@ -47,15 +65,29 @@ float avgScaleLevel = 1;
 int globMax;
 int thisMax;
 unsigned long globMaxTimer;
+//###################################################################
+//############### ПАРАМЕТРЫ РЕЖИМА colorWheel #######################
+int valueColorWheelMode = 0;
+unsigned long timeColorWheelMode = 0;
+long speedColorWheelMode = 0;
+unsigned int minSpeedColorWheelMode = 100;
+unsigned int brightnessRGB = 128;
+//###################################################################
 
-#define LOG_OUTPUT 1  // включить вывод лога в Serial port
-
+#define LOG_OUTPUT 0  // включить вывод лога в Serial port
+//######### Переменные #######
+int modeNum = 1;        // номер режима
+int modeNumCounts = 2;  // кол-во режимов
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 
+#ifdef USE_ENCODER
+  enc1.setType(TYPE1);
+  enc1.setFastTimeout(40);
+#endif
   strip.setDirection(REVERSE);
   strip.setBrightness(0);
 
@@ -68,8 +100,105 @@ void setup() {
   bitSet(ADCSRA, ADPS0);
 }
 
-// the loop routine runs over and over again forever:
+
 void loop() {
+
+  control();  // опрос элементов управления и изменение настроек
+  effects();  // отображение выбранного режима
+
+}
+
+void control() {
+  // обязательная функция отработки энкодера
+#ifdef USE_ENCODER
+  enc1.tick();
+
+  if (enc1.isClick()) {
+    modeNum++;
+    if (modeNum > modeNumCounts) {
+      modeNum = 1;
+    }
+    Serial.print("modeNum: "); Serial.println(modeNum);
+  }
+
+  // если поворачивается энкодер
+  if (enc1.isTurn()) {
+    switch (modeNum) {
+      case 1: // аудио режим, пока настроек здесь нет
+        if (enc1.isRightH()) brightnessRGB += 5;    // если было удержание + поворот направо, увеличиваем на 5
+        if (enc1.isLeftH()) brightnessRGB -= 5;     // если было удержание + поворот налево, уменьшаем на 5
+
+        if (brightnessRGB > 255) {
+          brightnessRGB = 255;
+        }
+
+        if (brightnessRGB < 0) {
+          brightnessRGB = 0;
+        }
+        break;
+
+      case 2: // режим цветового круга, настройка скорости
+
+        if (enc1.isRight()) speedColorWheelMode++;        // если был поворот направо, увеличиваем на 1
+        if (enc1.isLeft()) speedColorWheelMode--;         // если был поворот налево, уменьшаем на 1
+
+        if (enc1.isFastR()) speedColorWheelMode += 10;    // если был быстрый поворот направо, увеличиваем на 10
+        if (enc1.isFastL()) speedColorWheelMode -= 10;    // если был быстрый поворот налево, уменьшаем на 10
+
+        if (speedColorWheelMode > 5000) {
+          speedColorWheelMode = 5000;
+        }
+
+        if (speedColorWheelMode < minSpeedColorWheelMode) {
+          speedColorWheelMode = minSpeedColorWheelMode;
+        }
+
+        if (enc1.isRightH()) brightnessRGB += 5;    // если было удержание + поворот направо, увеличиваем на 5
+        if (enc1.isLeftH()) brightnessRGB -= 5;     // если было удержание + поворот налево, уменьшаем на 5
+
+        if (brightnessRGB > 255) {
+          brightnessRGB = 255;
+        }
+
+        if (brightnessRGB < 0) {
+          brightnessRGB = 0;
+        }
+        Serial.print("brightnessColorWheelMode: "); Serial.println(brightnessRGB);
+        Serial.print("speedColorWheelMode: "); Serial.println(speedColorWheelMode);
+        break;
+    }
+  }
+
+
+#endif
+}
+
+void effects() {
+  switch (modeNum) {
+    case 1:
+      audioMode();
+      break;
+
+    case 2:
+      colorWheelMode();
+      break;
+  }
+}
+
+void colorWheelMode() {
+  // режим цветового круга
+  if (micros() - timeColorWheelMode > speedColorWheelMode) {
+    valueColorWheelMode++;
+    if (valueColorWheelMode > 1530) {
+      valueColorWheelMode = 0;
+    }
+    strip.setBrightness(brightnessRGB);
+    strip.colorWheel(valueColorWheelMode);
+    timeColorWheelMode = micros();
+  }
+}
+
+void audioMode() {
 
   if (millis() - last_update_max_value > MAX_VALUE_UPDATE_TIME) {
     thisMax = 0;
@@ -92,8 +221,8 @@ void loop() {
 
     // изменяем яркость
     // масштабируем мгновенный максимум от общей средней громкости и общего среднего максимума к
-    // диапазону ШИМ 0-255
-    int scaleLevel = constrain(map(thisMax, avgLevel, avgLevelMax, 0, 255), 0, 255);
+    // диапазону ШИМ 0-brightnessRGB
+    int scaleLevel = constrain(map(thisMax, avgLevel, avgLevelMax, 0, brightnessRGB), 0, brightnessRGB);
 
     // сгладим этот полученный уровень
     avgScaleLevel +=  (scaleLevel - avgScaleLevel) * SMOOTH_SCALE_LEVEL;
@@ -102,7 +231,7 @@ void loop() {
     if (avgLevel < LOW_LEVEL) {
       avgScaleLevel = LOW_BRIGHTNESS;
     }
-    
+
     strip.setBrightness(avgScaleLevel);
 
     if (LOG_OUTPUT) {
